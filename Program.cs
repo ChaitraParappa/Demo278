81dfe04bb81f342f2d3d1a964f2d06e758745d9d
Chaitra <chaitra.p321@gmail.com>
2023-04-25 08:11:11 +0530

    It's a first File adding

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Console.WriteLine("Please enter the name");
+            // string username = Console.ReadLine();
+
+            // int a = 10;
+
+            // //UserName.Display();
+            //string  nameofuser= UserName.DisplayUserName(username,a);
+
+
+            // Console.WriteLine("The name is exceuted" + nameofuser);
+
+            // Console.WriteLine(UserName.a);
+
+            FlipCoin.DisplayPercentage();
+        }
+       
+       
+    }
+}
